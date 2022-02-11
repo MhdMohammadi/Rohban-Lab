3f5c0ecdbcc5b78075760bb600c375d7e2efbd0e
Mohammad Mohammadi <45603783+MhdMohammadi@users.noreply.github.com>
2022-02-11 05:56:24 +0330

    move to PGD

--- Parrallelized training/cornersearch_general_l0.py
+++ Parrallelized training/cornersearch_general_l0.py
@@ -486,7 +486,7 @@ if __name__ == '__main__':
 
     if args.pre_train == 'OFF':
         ref_net = utils.net_loader(args.net_arch, n_channels).to(device)
-        ref_net.load_state_dict(torch.load(pre_train_dir + '/model.pth'))
+        ref_net.load_state_dict(torch.load(pre_train_dir + '/pretrained88.pth'))
     else:
         ref_net = utils.net_loader(args.net_arch, n_channels).to(device)
         pre_train(ref_net, 2, pre_train_dir)
@@ -506,7 +506,7 @@ if __name__ == '__main__':
                 n_iter = num_example
 
                 train_directory = os.path.join(args.train_directory,
-                                               "l_" + str(lambda_val) + "_N_" + str(n_max) + "_e_" + str(n_iter))
+                                               "l_" + str(lambda_val) + "_N_" + str(n_max) + "_e_" + str(n_iter) + "PGD")
 
                 net = utils.net_loader(args.net_arch, n_channels)
                 net.load_state_dict(ref_net.state_dict())
