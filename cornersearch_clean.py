08fccef5491bda5c2929e0a5d22b0561e0a2f187
ce424-002-97110625 <m.mohammadi1378@outlook.com>
2022-08-20 19:51:37 +0430

    debug

--- cornersearch_clean.py
+++ cornersearch_clean.py
@@ -236,7 +236,8 @@ def train(net, num_epochs, train_dir):
 
             optimizer.zero_grad()
 
-            adv = attack(x_nat, y_nat)
+            # adv = attack(x_nat, y_nat)
+            adv = x_nat
 
             outputs = forward(adv)
 
