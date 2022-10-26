953505fbb176df3c658afec1a2150c8d42909021
mhdmohammadi <m.mohammadi1378@outlook.com>
2022-10-27 01:44:08 +0330

    update eval_clean

--- eval_clean.py
+++ eval_clean.py
@@ -4,7 +4,7 @@ import torch
 # import torchvision
 # import torchvision.transforms as transforms
 
-# import torch.nn as nn
+import torch.nn as nn
 # import torch.nn.functional as F
 
 # import torchvision.models as torch_models
