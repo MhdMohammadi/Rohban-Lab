7e7a59a46bb14c8feae1e0ea5039b87ff8a96220
mhdmohammadi <m.mohammadi1378@outlook.com>
2022-10-27 03:54:50 +0330

    debug

--- cornersearch_clean.py
+++ cornersearch_clean.py
@@ -229,10 +229,11 @@ def train(net, num_epochs, train_dir):
 
             # C H W
             x_nat, y_nat = data[0].to(device), data[1].to(device)
+            print(x_nat.min(), x_nat.max(), y_nat.min(), y_nat.max())
 
             # H W C
             x_nat = x_nat.permute(0, 2, 3, 1).to(device)
-
+            
             optimizer.zero_grad()
 
             # TODO
