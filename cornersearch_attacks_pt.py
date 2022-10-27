184cad1ce98a6561ff5ecc6cadb32fdc4431cd5b
mhdmohammadi <m.mohammadi1378@outlook.com>
2022-10-27 10:53:51 +0330

    debug

--- cornersearch_attacks_pt.py
+++ cornersearch_attacks_pt.py
@@ -269,9 +269,9 @@ class CSattack():
         print('-- start storing the adversarial data --')
         os.makedirs('adversarial_images', exist_ok=True)
         for i in range(adv.shape[0]):
+            adv[i][10] = 0
             plt.imsave(f'adversarial_images/img_{i}_pixel_{pixels_changed[i]}.jpg', adv[i])
             plt.imsave(f'adversarial_images/img_{i}.jpg', x_nat[i])
-            adv[0][10] = 0
             print((np.abs(adv[i] - x_nat[i]) > 1e-10).sum())
         print('-- the batch is successfully stored --')
             
